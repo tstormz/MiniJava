6026aa674f4a4ebf0de64c8eb35d3057aa8118c7
tstorm <travisdhafner@gmail.com>
2016-11-26 21:02:46 -0500

    loop

--- src/main/java/com/tstorm/compiler/rules/statements/Loop.java
+++ src/main/java/com/tstorm/compiler/rules/statements/Loop.java
@@ -1,12 +1,15 @@
 package com.tstorm.compiler.rules.statements;
 
+import com.tstorm.compiler.assembler.Assembler;
 import com.tstorm.compiler.rules.expressions.Expression;
 import com.tstorm.compiler.typechecker.Visitor;
+
+import java.io.PrintWriter;
 
 /**
  * Created by tstorm on 10/31/16.
  */
-public class Loop implements Statement {
+public class Loop extends Assembler implements Statement {
 
     private final Expression expression;
     private final Statement statement;
@@ -31,5 +34,17 @@ public class Loop implements Statement {
     @Override
     public boolean accept(Visitor v) {
         return v.visit(this);
+    }
+
+    @Override
+    public void generateCode(PrintWriter out) {
+        Label loop = generateLabel();
+        out.println(loop.getLabel() + " ; test expr");
+        ((Assembler) expression).generateCode(out);
+        Label quit = generateLabel();
+        out.println("ifeq " + quit.getLabelUse() + " ; break");
+        ((Assembler) statement).generateCode(out);
+        out.println("goto " + loop.getLabelUse() + " ; loop");
+        out.println(quit.getLabel());
     }
 }
